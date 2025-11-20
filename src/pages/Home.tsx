bd7c0a25512f2bbadd0c37ad6bf18ef777949451
Raihan Rony <91003324+Moskov-1@users.noreply.github.com>
2025-11-20 19:35:41 +0600

    custom css 1

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return (
+        <>
+            <div className="min-h-screen">Home
+
+            {/* Theme toggle */}
+            {/* Shooting Star Background Effect */}
+            {/* Navbar */}
+            {/* MAIN Content */}
+            {/* Footer Content */}
+            </div>
+        </>
+    )
+} 
